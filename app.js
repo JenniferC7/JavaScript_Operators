950ebf2c010c8c2582a947c292437d313e7f81bb
jennifer cox <jenncox97@gmail.comgit config --global user.email jenncox97@gmail.com>
2023-09-17 23:26:08 -0400

    done

--- app.js
+++ app.js
@@ -7,13 +7,44 @@ console.log(
 console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
+let a=20;
+let b=4;
+let add=a+b;
+let minus=a-b;
+let multiply=a*b;
+let divide=a/b;
+console.log(divide)
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
 // YOUR CODE HERE
-
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+1.1111 console.log(`num+str`)
+2.11eleven console.log(`num+str2`)
+3.12 console.log(`num+isPresent`)
+4.Frodo11 console.log(`firstName+num`)
+5.true11 console.log(`isPresent+str`)
+6.Frodo Baggins console.log(`firstName+lastName`)
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
+1.true console.log(`val==str3`)
+2.false console.log(`val===str3`)
+3.true console.log(`!isAwake`)
+4.false console.log (`"eleven"==str4&&val>=str3`)
+5.true console.log(`!isAwake||isAwake`)
+6.true console.log(`0==false`)
+7.false console.log(`0===false`)
+8.false console.log(`0!=false`)
+9.true console.log(`0!==false`)
